2497d86dd367aa33934102934bdf8b5c42e1ab1c
whn <me@whn.se>
2020-06-17 18:14:27 +0200

    add support for compgraph

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM tiangolo/uvicorn-gunicorn-fastapi:python3.8
 COPY Pipfile Pipfile.lock /app/
 WORKDIR /app
 RUN pip install pipenv
-RUN pipenv install --deploy
+RUN pipenv install --deploy --system
 COPY actions /app/actions
 CMD ["uvicorn", "--host", "0.0.0.0", "actions.main:app"]
--- actions/api/internal.py
+++ actions/api/internal.py
@@ -1,3 +1,7 @@
+# coding: utf-8
+
+import os
+
 from typing import Any, Dict, Tuple
 
 from fastapi import APIRouter, Depends
@@ -5,6 +9,7 @@ from pydantic import BaseModel
 from fastapi.responses import ORJSONResponse
 
 from actions.actions import identity
+from actions.deps import http_client
 
 router = APIRouter()
 
@@ -13,9 +18,19 @@ class ActionRequest(BaseModel):
     properties: Dict[str, Any]
 
 
+COMPGRAPH_BASE_URL = os.environ["COMPGRAPH_BASE_URL"]
+
+
+async def post_action(http_client, url, properties: Dict[str, Any]):
+    res = http_client.post(url, json=properties)
+    await res.raise_for_status()
+    return await res.json()
+
 @router.post("/compute")
-async def compute_internal_action(action_request: ActionRequest) -> Dict[str, Any]:
+async def compute_internal_action(action_request: ActionRequest, http_client = Depends(http_client)) -> Dict[str, Any]:
     # TODO: implement actions 😛
     if action_request.kind == 'identity':
         return identity(action_request.properties)
+    elif action_request.kind == "TriggerDag":
+        return await post_action(http_client, f"{COMPGRAPH_BASE_URL}/triggerProcess", action_request.properties)
     return {}
